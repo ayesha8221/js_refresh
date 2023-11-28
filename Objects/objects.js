b4df1730266efb27793585a725b89395b63ef81e
ayesha8221 <ayesha.glnt012@gmail.com>
2023-11-28 15:19:28 +0200

    date

--- Objects/objects.js
+++ Objects/objects.js
@@ -164,3 +164,15 @@ Thank you for joining my mailing list.
 
 Regards 
 Ayesha`;
+
+
+
+
+// DATE 
+
+const now = new Date();
+const date1 = new Date()
+const date1 = new Date('May 11 2018 09:00');
+const date2 = new Date(2018, 4, 11, 9);
+
+now.setFullYear(2017);
