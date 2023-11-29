f26929eaa4e23528ebe7a86f649eb12a3f33a6fb
ayesha8221 <ayesha.glnt012@gmail.com>
2023-11-29 15:28:09 +0200

    used  object literal syntax

---
+++ Objects/BlogPost.js
@@ -0,0 +1,24 @@
+// create a blog post
+// use object literal syntax
+
+const blogPost= {
+    title: 'Ayeshas Opinions',
+    body: 'blah blah blah',
+    author: 'Ayesha Galant',
+    views: 10000000,
+    comments: [
+        {
+            author: 'Brittany Spears',
+            body: 'so inspiring bsf' 
+        },
+        {
+            author: 'Michelle Obama',
+            body: 'yaaaaaaasss'
+        }
+       
+    ],
+       
+    isLive: true
+}
+
+console.log(blogPost);
